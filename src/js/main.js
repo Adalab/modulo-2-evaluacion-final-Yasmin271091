41758e4f2846a0c2121e20cbe849c8a24092fdcc
Yasmin <yasmin27101991@gmail.com>
2023-09-29 08:58:25 +0200

    starting with events and functions

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,16 @@
 //VARIABLES Y CONSTANTES
 
 
+const searchText = document.querySelector('.js-input-text');
+const searchButton = document.querySelector('.js-search-btn');
+
+const container = document.querySelector('.js-list');
+const favorites = document.querySelector('.js-fav-list');
+
+const img = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+
+let series = [];
+let favoritesSeries = [];
+
+//conseguir los datos de la Api
+
